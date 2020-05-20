699959ebd3b79f02928dd0d46a5719e6af3ad907
geekboy81 <superstar.geekboy@gmail.com>
2020-05-21 07:36:25 +0800

    use firestore

--- functions/api/todos.js
+++ functions/api/todos.js
@@ -1,16 +1,56 @@
-exports.getAllTodos = (req, res) => {
-  todos = [
-    {
-      id: 1,
-      title: 'greeting',
-      body: 'Hello world from jekas'
-    },
-    {
-      id: 2,
-      title: 'greeting2',
-      body: 'Hello2 world2 from jekas'
-    }
-  ];
+const { db } = require('../utils/admin');
 
-  return res.json(todos);
+exports.getAllTodos = (req, res) => {
+  db
+    .collection('todos')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let todos = [];
+			data.forEach((doc) => {
+				todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+					body: doc.data().body,
+					createdAt: doc.data().createdAt,
+				});
+      });
+
+      return res.json(todos);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ err: err.code })
+    })
+}
+
+exports.postOneTodo = (req, res) => {
+  console.log('req.body without body-parser', req.body);
+
+  if (req.body.body.trim() === '') {
+		return res.status(400).json({ body: 'Must not be empty' });
+  }
+
+  if (req.body.title.trim() === '') {
+    return res.status(400).json({ title: 'Must not be empty' });
+  }
+
+  const newTodoItem = {
+    title: req.body.title,
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+  };
+
+  db
+    .collection('todos')
+    .add(newTodoItem)
+    .then(doc => {
+      const responseTodoItem = newTodoItem;
+      responseTodoItem.id = doc.id;
+      return res.json(responseTodoItem);
+    })
+    .catch(err => {
+      console.err(err);
+      return res.status(400).json({ err: 'Something went wrong' });
+    })
 }
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
-const { getAllTodos } = require('./api/todos');
+const {
+  getAllTodos,
+  postOneTodo,
+} = require('./api/todos');
 
 app.get('/todos', getAllTodos);
+app.post('/todos', postOneTodo);
 
 exports.api = functions.https.onRequest(app);
